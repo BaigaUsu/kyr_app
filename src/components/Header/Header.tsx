532fc336d8d306585d11dcbef39babbe4bd1b3d2
BaigaUsu <baigazy7777@gmail.com>
2024-09-13 20:15:35 +0600

    temporarily commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>KyrgyzApp</div>
-            <div className={styles.lang}>
+            {/* <div className={styles.lang}>
                 <select>
                     <option value="en">English</option>
                     <option value="ru">Русский</option>
                     <option value="kg">Кыргызча</option>
                 </select>
-            </div>
+            </div> */}
         </header>
     )
 }
